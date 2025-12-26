9977be33488885340e9d15ca07b00cbc6906c7aa
unknown <mamiaachance@gmail.com>
2025-12-26 12:05:47 +0100

    Firts commit Branch

---
+++ Core/datasets.py
@@ -0,0 +1,46 @@
+def load_csv (file_path, separator = ',', encoding = 'utf-8'):
+    with open(file_path, 'r', encoding) as file:
+        file_list = []
+        for line in file:
+            line = line.strip()
+            if line == "":
+                continue
+            sep = line.split(separator)
+            file_list.append(sep)
+            return file_list
+
+def load_json(file_path):
+    with open(file_path, 'r', encoding = 'utf-8') as _json:
+        file_list = []
+        in_chain = False
+        temp_json_text = ""
+        text = _json.read()
+        text = text.strip()
+        if text[0] == "[":
+            if text[-1] == "]":
+                print("liste JSON, traitement en cours...")
+                for c in text:
+                    if c == "\"":
+                        temp_json_text += c
+
+
+
+
+
+def validateData(strict = True):
+
+
+def cleanData (strategy='remove'):
+
+
+def filterBy (condition: callable):
+
+
+def normalize(methods = 'minmax', column = 'none'):
+
+
+def getStats():
+
+
+def exportCsv(file_path):
+
---
+++ Core/statistics.py
@@ -0,0 +1,49 @@
+import math
+
+def mean(data_list):
+    if len(data_list) == 0:
+        raise ValueError("La liste est vide !")
+    return math.fsum(data_list) / len(data_list)
+
+def variance(data_list, samples = True):
+    if len(data_list) == 0:
+        raise ValueError("Liste vide!")
+    _mean = mean(data_list)
+    total = math.fsum((li - _mean)**2 for li in data_list)
+    if samples:
+        return total / len(data_list) - 1
+    else :
+        return total / len(data_list)
+
+def std_dev(data_list, samples = True):
+    if len(data_list) == 0:
+        raise ValueError("Liste VIde !")
+    return math.sqrt(variance(data_list, samples))
+    
+
+def covariance(x, y):
+    if len(x) != len(y):
+        raise ValueError("Impossible de calculer la COV dans ces conditions")
+    
+    mean_x = mean(x)
+    mean_y = mean(y)
+
+    return math.fsum((li - mean_x) * (lu - mean_y) for li,lu in zip(x,y)) / (len(x) - 1)
+
+def correlation(x, y):
+    varX = std_dev(x)
+    varY = std_dev(y)
+    if varX == 0 or varY == 0:
+        raise ValueError("Math Error")
+    return covariance(x,y) / (varX * varY)
+
+
+def correlation_matrix(data):
+    var = list(data[0])
+    matrix = {v: {} for v in var}
+
+    for v1 in var:
+        for v2 in var:
+            list1 = [row[v1] for row in data]
+            list2 = [row[v2] for row in data]
+            matrix[v1][v2] = correlation(list1, list2)
